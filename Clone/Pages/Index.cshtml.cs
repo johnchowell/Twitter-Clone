d1ecd3142f9aeedab064adeee3cd3d70fb8670ac
John (JC) Howell <113290014+FriggityFresher@users.noreply.github.com>
2023-07-24 21:54:51 -0500

    Login UI Complete
    Sign-up page also added.

--- Clone/Pages/Index.cshtml.cs
+++ Clone/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Data;
 
 namespace Clone.Pages;
 
@@ -14,6 +18,19 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
+        InitiateDatabase();
+    }
 
+    public void InitiateDatabase()
+    {
+        string conString = "Data Source=database.db; version=3";
+        using (SQLiteConnection con = new SQLiteConnection(conString))
+        {
+            con.Open();
+            string sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(20), email VARCHAR(20), password VARCHAR(20))";
+            SQLiteCommand cmd = new SQLiteCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
     }
 }
